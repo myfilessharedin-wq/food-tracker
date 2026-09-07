61021ffdc8e67cf1870b988deb01314900db8e71
myfilessharedin-wq <myfilessharedin@gmail.com>
2026-09-07 15:25:31 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKDBkruxepuRR6zMMZdTGX0X-KF1yfuvo",
+  authDomain: "food-84188.firebaseapp.com",
+  projectId: "food-84188",
+  storageBucket: "food-84188.firebasestorage.app",
+  messagingSenderId: "476560005208",
+  appId: "1:476560005208:web:b1a3e56f8564cf22c4d22d",
+  measurementId: "G-R53GE9MFGT"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
+
